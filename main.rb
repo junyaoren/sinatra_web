128a1021a2b21c54112f88b9481aa1cc0b10a851
junyaoren <renjunyao@gmail.com>
2019-05-20 00:52:53 -0700

    style

--- main.rb
+++ main.rb
@@ -3,7 +3,28 @@ require 'sinatra/reloader' if development?
 
 class ApplicationController < Sinatra::Base
 
+	configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, 'password_security'
+  end
+
 	get '/' do
-	  "hello to junyao's world"
+		erb :index
+	  
 	end
+
+
+
+	helpers do
+    def current_user
+        @current_user ||= User.find_by(:id => session[:user_id]) if session[:user_id]
+    end
+
+    def logged_in?
+        !!current_user
+    end
+  end
+
 end
---
+++ student_controller.rb
@@ -0,0 +1,5 @@
+require 'sinatra'
+
+class StudentController < ApplicationController
+
+end
