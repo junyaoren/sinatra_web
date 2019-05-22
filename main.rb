dacb62b254e56b3b76de036f39cec9f3f8433a40
junyaoren <renjunyao@gmail.com>
2019-05-22 04:38:23 -0700

    configure

--- main.rb
+++ main.rb
@@ -1,3 +1,11 @@
+configure :development, :test do 
+  DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+end
+
+configure :production do 
+  DataMapper.setup(:default,ENV['DATABASE_URL']) 
+end
+
 require 'sinatra'
 require 'sass'
 require_relative 'user'
@@ -6,13 +14,7 @@ require_relative 'comment'
 #require_relative 'database_seed'
 require 'sinatra/reloader' if development?
 
-configure :development, :test do 
-  DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
-end
 
-configure :production do 
-  DataMapper.setup(:default,ENV['DATABASE_URL']) 
-end
 
 configure do
 set :public_folder, './public'
