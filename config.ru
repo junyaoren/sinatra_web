71233fa37e68c38d9decf768feeb93e4999b106b
junyaoren <renjunyao@gmail.com>
2019-05-23 02:14:11 -0700

    a

--- config.ru
+++ config.ru
@@ -1,12 +1,3 @@
 require_relative "./main"
 
-configure :development, :test do 
-  DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
-end
-
-configure :production do 
-  DataMapper.setup(:default,ENV['DATABASE_URL']) 
-end
-
-
 run Sinatra::Application
--- main.rb
+++ main.rb
@@ -1,10 +1,22 @@
 require 'sinatra'
+require 'sinatra/reloader' if development?
 require 'sass'
+
+configure :development, :test do 
+  DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+end
+
+configure :production do 
+  DataMapper.setup(:default,ENV['DATABASE_URL']) 
+end
+
+
+
 require_relative 'user'
 require_relative 'student'
 require_relative 'comment'
 require_relative 'database_seed'
-require 'sinatra/reloader' if development?
+
 
 
 
